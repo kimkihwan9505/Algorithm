473ea5ec7f1da3af6645f74ee8bc0330e0ddf73b
kimkihwan9505 <95116074+kimkihwan9505@users.noreply.github.com>
2022-01-04 00:04:40 +0900

    Delete 10250.py

--- Baekjoon/10250.py
+++
@@ -1,9 +0,0 @@
-f = int(input())
-for _ in range(f):
-    a,b,c = map(int,input().split())
-    floor = c%a # 101,201,301~601,102,202,302,402 에서 층만
-    room=c//a +1 # 101,201,301~601,102,202,302,402 10//6 =1 1호실은 꽉찼으니까 다음호실
-    if floor ==0 :
-        floor=a
-        room=c//a
-    print(floor*100+room)
